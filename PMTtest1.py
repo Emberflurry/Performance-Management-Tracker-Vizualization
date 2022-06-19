2c615c999430803e596b603366d20494ab4e72fe
John DeForest <juandelbosque373@gmail.com>
2022-06-19 03:18:09 -0400

    fixing labeling for multitest, adding attempt of actual PMT tool conversion
    multitest: how to make labeling dependent/dynamic to whichever plot is hovered? do i need to?
    
    PMTtest1: read pre-calcd PMT values from excel db or redo formulas? currently just going to read...need to change up the PD DF creation a bit n hardcode it rather than pulling from wklydfcleaner2.py as multitest does

--- PMTtest1.py
+++ PMTtest1.py
@@ -6,12 +6,64 @@ from plotly.subplots import make_subplots
 import plotly.io as pio
 import plotly.express as px
 from wklydfcleaner2 import dfretrnclen
+import pandas as pd
+from datetime import datetime, date
 
 # set source excel file and sheet to pull from, run retriever and cleaner to prepare the df
 sourceexcelfile = r'C://Users//John DeForest//Desktop//Ski Projects and Analysis//PMT_3_41_BACKUP.xlsx'
-sourceexcelsheet = 'weeklystats'
-mydf = dfretrnclen(sourceexcelfile, sourceexcelsheet)
-#print(mydf.head())
+sourceexcelsheet = 'TrainingData'  # try to pull ALREADY CALCD BY EXCEL PMT VALS first (in future redo formulas?)
+
+# READ EXCEL INTO PANDAS DF:
+print("getting date...")
+today = date.today().strftime("%m/%d/%Y").split('/')
+tyear = int(today[2])
+tmonth = int(today[0])
+tday = int(today[1])
+# print(tyear,"/",tmonth,"/",tday)
+# print(tyear)
+
+# load df
+print("loading dataframe...")
+mydf = pd.read_excel(sourceexcelfile, sheet_name=sourceexcelsheet)
+
+# round the values (NOT the dates)
+print("cleaning values...")
+mydf.wklyhrs = mydf.wklyhrs.round(1)
+mydf.wklyTSStot = mydf.wklyTSStot.round(1)
+mydf.TSSperhr = mydf.TSSperhr.round(1)
+
+# get rid of empty entries n stuff earlier (farther back in time than log entries)
+print("emptying the trash...")
+for i, r in mydf.iterrows():
+    if r['wklyhrs'] > 0 or r['wklyTSStot'] > 0 or r['TSSperhr'] > 0:
+        # print(df2.iloc[i])
+        break
+    elif r['wklyhrs'] <= 0 or r['wklyTSStot'] <= 0 or r['TSSperhr'] <= 0:
+        mydf.drop(index=i, inplace=True)
+
+# get rid of all weekly entries yet to come
+for i, r in mydf.iterrows():
+    vals = str(r['datewkstart'])[0:10].split('-')
+    day = int(vals[2])
+    month = int(vals[1])
+    year = int(vals[0])
+# print(year, month, day)
+
+    if datetime(year, month, day) > datetime(tyear, tmonth, tday):
+        mydf.drop(index=i, inplace=True)
+
+# print(df2.head())
+# print(df2.tail())
+# check df for correct start and end points + values
+
+# convert and display
+print("melting dataframe...")
+mydf = pd.melt(mydf, id_vars=['datewkstart', 'wklyhrs', 'wklyTSStot'], value_vars=mydf.columns[3:], var_name='category', value_name='TSSperHOUR')
+# print(df2.head())
+
+# ^ READING AND CLEANING INTO PD DF
+# _________________________________________________________________________________________
+# v CREATION OF FIGURES N PLOTS
 
 fig2 = make_subplots(specs=[[{"secondary_y": True}]])
 
