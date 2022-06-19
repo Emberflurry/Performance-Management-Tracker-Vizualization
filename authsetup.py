526f6eaa149f9d748e224db1409a9460a7872ca5
John DeForest <juandelbosque373@gmail.com>
2022-06-19 03:26:41 -0400

    removal of cluttered early testing and auth files
    unnecessary unless they are...will find out shortly...

--- authsetup.py
+++
@@ -1,5 +0,0 @@
-
-import chart_studio
-
-chart_studio.tools.set_credentials_file(username='Emberflurry', api_key='iUhE0KmThcaVXoUkr2J5')
-
--- onlinebasictest.py
+++
@@ -1,9 +0,0 @@
-import chart_studio.plotly as py
-import plotly.graph_objects as go
-
-
-trace0 = go.Scatter(x=[1, 2, 3, 4], y=[10, 15, 13, 17])
-trace1 = go.Scatter(x=[1, 2, 3, 4], y=[16, 5, 11, 9])
-data = [trace0, trace1]
-
-py.plot(data, filename='basic-line', auto_open=True)
