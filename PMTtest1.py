b73e2206c0eba55e82fc0ac99cc3489894d65a71
John DeForest <juandelbosque373@gmail.com>
2022-07-01 04:49:19 -0400

    PMT now writes to Chart Studio server for accessibility

--- PMTtest1.py
+++ PMTtest1.py
@@ -126,6 +126,6 @@ fig2.update_yaxes(title_text="<b>TSB", secondary_y=True, range=(-75, 35),  gridc
 #         ser['hovertemplate'] = 'totalhours: %{y} <br> TSS/hr: %<br> date(start of week): %{x}<extra></extra>'
 
 # print("writing to display page...")
-py.plot(fig2,filename='dailyPMTtest1',auto_open=True) 
-#pio.write_html(fig2, file='dailyPMTtest1.html', auto_open=True)  # local HTML write, no internet or chart studio used
+py.plot(fig2, filename='dailyPMTtest1', auto_open=True)  # 2web Chart Studio write/post, allows embedding on websites
+# pio.write_html(fig2, file='dailyPMTtest1.html', auto_open=True)  # local HTML write, no internet or chart studio used
 # print("done! (plz work oh god)")
